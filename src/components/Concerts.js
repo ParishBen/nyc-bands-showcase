0272df80defe3ff60706573e1c73eaf0bf00fe13
Ben <parishben13@gmail.com>
2021-09-18 23:41:08 -0400

    Cleaned up Concerts.js code

--- src/components/Concerts.js
+++ src/components/Concerts.js
@@ -15,10 +15,10 @@ class Concert extends React.Component{
     
     
     componentDidMount(){
-        this.newfetchmcgee()
+        this.ticketMasterFetch()
     }
     
-newfetchmcgee = () => {
+ticketMasterFetch = () => {
         fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${this.props.name}&classificationName=music&locale=en-us&size=50&apikey=JcHBGDzcTW1CwZ7zgfznR4koIFNm8zmn`)  
     .then(resp=> resp.json())
         .then(eventObj=> {  
@@ -26,10 +26,6 @@ newfetchmcgee = () => {
                  } else { 
                   if(eventObj._embedded.events.length > 0){
                       this.setState({ events: eventObj._embedded.events })
-                // eventObj._embedded.events[0]._embedded.venues[0].name,
-                // city: eventObj._embedded.events[0]._embedded.venues[0].city.name,
-                // country: eventObj._embedded.events[0]._embedded.venues[0].country.name,
-                // url: eventObj._embedded.events[0].url
             }}
         })
         .then(() => {
@@ -47,37 +43,29 @@ eventsParse = (concerts) => {
           if(event._embedded.attractions){
              if( event._embedded.attractions.find(artist => {
                 return artist.name.toLowerCase() === artname.toLowerCase()
-               }))
-              {
+                })
+               ){
                   return event
                 }  
             }
-            //return funcerts
         }) 
-        this.setState({filteredConcerts: funcerts }) //funcerts
-        // let truConcerts = element._embedded.attactions.filter(artist => {
-        //     return artist.name.toLowerCase() == artname.toLowerCase()
+        this.setState({filteredConcerts: funcerts }) 
+        
      }     
     }
-        //return truConcerts
-    //});
+        
 
     createList = (list) => {
-        //this.state.filteredConcerts && console.log(this.state.filteredConcerts.length)
         if (list.length){
             if(this.state.filteredConcerts.length > 5){           
                  let listArr = this.sorter(this.state.filteredConcerts)
                  listArr = listArr.map( concert => {
                     return <li key={`${concert.name+" - "+concert._embedded.venues[0].city.name}`}><span style={{background:'lightgray', color:'black'}}>Date: {concert.dates.start.localDate} || Concert : {concert.name} || Location: {concert._embedded.venues[0].city ? concert._embedded.venues[0].city.name : concert._embedded.venues[0].address ? concert._embedded.venues[0].address.line1 : 'No City Address Listed'} <a href={concert.url} target={'_blank'} rel={'noreferrer'}> - Buy Tickets</a></span></li> 
-                    //return <li key={this.state.filteredConcerts[i].name}>Date: {this.state.filteredConcerts[i].dates.start.localDate} || Concert : {this.state.filteredConcerts[i].name} || Location: {this.state.filteredConcerts[i]._embedded.venues[0].city ? this.state.filteredConcerts[i]._embedded.venues[0].city.name : this.state.filteredConcerts[i]._embedded.venues[0].address ? this.state.filteredConcerts[i]._embedded.venues[0].address.line1 : 'No City Address Listed'} <a href={this.state.filteredConcerts[i].url} target={'_blank'} rel={'noreferrer'}> - Buy Tickets</a></li> 
                  })
                  listArr.length = 5
                  return listArr
                 } else {
-       // let count = this.state.filteredConcerts.length;
-        //for(let i=0; i<count; i++){
-           // while (count > 0 ){
-          // count--
+       
              return  this.sorter(this.state.filteredConcerts).map( concert => {
              return <li key={concert.name}><span style={{background:'lightgray', color:'black'}}> Date: {concert.dates.start.localDate} || Concert : {concert.name} || Location: {concert._embedded.venues[0].city ? concert._embedded.venues[0].city.name : concert._embedded.venues[0].address ? concert._embedded.venues[0].address.line1 : 'No City Address Listed'} <a href={concert.url} target={'_blank'} rel={'noreferrer'}> - Buy Tickets</a></span></li> 
             })
@@ -114,5 +102,3 @@ sorter = (eventArr) => {
 }
 export default Concert
 
-// // this.state.city && this.state.city, this.state.country && this.state.country)}
-    /* //* Venue: {this.state.event && this.state.event  ? this.state.event : 'NO CONCERT SCHEDULED'} City: { this.state.city && this.state.city  ? this.state.city : 'City Not Listed'} Country: {this.state.country && this.state.country ? this.state.country : 'Country Not Listed'}<a href={this.state.url} target="_blank" rel="noopener noreferrer">Tickets</a> */ 
