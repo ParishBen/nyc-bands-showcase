de38eaefbdff541dd60fee1006d2dbc26974554c
Benjamin Parish <parishben13@gmail.com>
2022-01-27 23:52:58 -0500

    Refined SpotifyFetch Component

--- src/components/SpotifyFetch.js
+++ src/components/SpotifyFetch.js
@@ -5,15 +5,14 @@ import {deco} from '../containers/App';
 import { decodeToken } from "react-jwt";
 
 
-const properCase =              // this goes through the imported list of Bands formed in NYC. Then URI encodes the non-alphabetical characters for the Fetch Request.
-nycBands.map(artist => {
-  return artist.replaceAll(' ','%20').replaceAll("'","%27").replaceAll(":", "%3A").replaceAll('.',"+.")
-})
+const properCase =   // this goes through the imported list of Bands formed in NYC. Then URI encodes the non-alphabetical characters for the Fetch Request.
+  nycBands.map(artist => {
+    return artist.replaceAll(' ','%20').replaceAll("'","%27").replaceAll(":", "%3A").replaceAll('.',"+.")
+  })
 
 const artistArrReducer = (newArt, artArray) => {    // Takes in the Artist & Array & will check the array if the Artist Object is already in the Array based on unique ID.
   return artArray.find(artist=> artist.id === newArt.id)
 }
-
 
 export default class SpotifyFetch extends React.Component { 
   
@@ -29,26 +28,26 @@ export default class SpotifyFetch extends React.Component {
        return deco()
      } else {
          let newTok = window.localStorage.getItem('access_token')    // OR grabs it from the LocalStorage / Decodes it for the Token Value
-         if (newTok){
-         const myDecodedToken =  decodeToken(newTok);
-      return myDecodedToken.spotify_token}
-     }
-   }
+           if (newTok){
+             const myDecodedToken =  decodeToken(newTok);
+             return myDecodedToken.spotify_token
+           }
+       }
+    }
     
     
     componentDidMount(){
       if(this.state.artists.length < 140){
       this.allNycBandsFetch()
       }
-
     }
 
   allNycBandsFetch = () => {                                             
         let artistsObjArr = []; // this will hold Every Artist object return
         console.log('in the Fetch', this.decoVal())
-        for (let i=0; i< properCase.length; i++){    // this loops through each uri encoded band-name & does get Fetch to Spotify
-          fetch(`https://api.spotify.com/v1/search?q=${properCase[i]}&type=artist`, {
-            headers: {
+         for (let i=0; i< properCase.length; i++){    // this loops through each uri encoded band-name & does get Fetch to Spotify
+           fetch(`https://api.spotify.com/v1/search?q=${properCase[i]}&type=artist`, {
+             headers: {
                 'Content-Type':'application/json',
                  Accept:'application/json',
                  "Authorization": `Bearer ${this.decoVal()}`    //  TokenValue=> To make a Fetch to Spotify to return artist data
@@ -58,23 +57,23 @@ export default class SpotifyFetch extends React.Component {
               .then(artObjs=> {  
                 if(artObjs.error){
                   console.log(artObjs, " ",artObjs.error)
-                  if(artObjs.error.message === ("Invalid access token" || "The access token expired")){
-                    return window.location ="http://localhost:8888/login"
-                  }
+                    if(artObjs.error.message === ("Invalid access token" || "The access token expired")){
+                      return window.location ="http://localhost:8888/login"
+                    }
                   if(artObjs.error.status === 429){
                     return window.location.reload()
                   }
                 }
-                   else {               // going to put this artist obj(s) response into another function which returns correct artist obj from results
-                      let foundArtist = artObjs.artists.items        // this returns artist(s) item's array from Spotify
-                        if (foundArtist && `${foundArtist.length}` > 0){    // if response isn't undef & has any length
-                          let realArtist = foundArtist.find( artist => artist.name === decodeURI(properCase[i])) // find artist in array of objects with the exact name matching the initial unencoded band name.
-                            if (realArtist.name === "Run-DMC"){   //Exception Case for Artist=> title: Run-D.M.C. posed problems in fetch due to '.' Will grab correct Artist.id now.
-                              realArtist = foundArtist.find(artist => artist.id === "3CQIn7N5CuRDP8wEI7FiDA")   //Setting exception case ID for Run-D.M.C.
-                      }
-                        if (realArtist !== undefined){         
-                          if(!artistArrReducer(realArtist, artistsObjArr)){ // REDUCER - if we have a realArtist value it checks if that Artist object is in Array. IF not present will push to array.
-                             artistsObjArr.push(realArtist)
+                else {               // going to put this artist obj(s) response into another function which returns correct artist obj from results
+                   let foundArtist = artObjs.artists.items        // this returns artist(s) item's array from Spotify
+                     if(foundArtist && `${foundArtist.length}` > 0){    // if response isn't undef & has any length
+                       let realArtist = foundArtist.find( artist => artist.name === decodeURI(properCase[i])) // find artist in array of objects with the exact name matching the initial unencoded band name.
+                          if(realArtist.name === "Run-DMC"){   //Exception Case for Artist=> title: Run-D.M.C. posed problems in fetch due to '.' Will grab correct Artist.id now.
+                             realArtist = foundArtist.find(artist => artist.id === "3CQIn7N5CuRDP8wEI7FiDA")   //Setting exception case ID for Run-D.M.C.
+                          }
+                         if (realArtist !== undefined){         
+                        if(!artistArrReducer(realArtist, artistsObjArr)){ // REDUCER - if we have a realArtist value it checks if that Artist object is in Array. IF not present will push to array.
+                          artistsObjArr.push(realArtist)
                         }
                       }
                     }
@@ -97,8 +96,8 @@ export default class SpotifyFetch extends React.Component {
        delayed = setTimeout(() => {              // Will reFetch all the bands after 8 seconds IF Spotify didn't return all the bands properly
         if(this.state.artists.length < 140){
             this.allNycBandsFetch()
-          } 
-        }, 8000)
+         } 
+       }, 8000)
 
     render(){
 //Invisible Fetch- to push Artists to App.js state
