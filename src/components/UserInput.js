2693c178248089b2e95a080c6a1126b9c29fb767
Benjamin Parish <parishben13@gmail.com>
2022-01-28 00:13:48 -0500

    Refined UserInput Component

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -6,26 +6,26 @@ class UserInput extends React.Component {
   
     constructor(){
       super();
-      this.state = {
-        name:'',
-        email: '',
-        password: '',
-      }
+       this.state = {
+          name:'',
+          email: '',
+          password: '',
+       }
     }
     
      artBackGround = () => {
-      let src = document.createElement('img')
-      src.id = 'UserLanding-container-img'
-      src.url = 'https://images.pexels.com/photos/3742472/pexels-photo-3742472.jpeg?cs=srgb&dl=pexels-rombo-3742472.jpg&fm=jpg'
-     document.querySelector('body').style.background = `url("${src.url}") no-repeat fixed center`
-     document.querySelector('body').style.backgroundSize = 'cover'
-    }
+        let src = document.createElement('img')
+         src.id = 'UserLanding-container-img'
+          src.url = 'https://images.pexels.com/photos/3742472/pexels-photo-3742472.jpeg?cs=srgb&dl=pexels-rombo-3742472.jpg&fm=jpg'
+         document.querySelector('body').style.background = `url("${src.url}") no-repeat fixed center`
+        document.querySelector('body').style.backgroundSize = 'cover'
+     }
 
-handleUserInfoChange(event) {                 // Signing in to Landing Page handling value changes
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
+  handleUserInfoChange(event) {                 // Signing in to Landing Page handling value changes
+      this.setState({
+        [event.target.name]: event.target.value,
+      });
+    }
   
   userSerializer = (user) => {
     let currentUser = { 
@@ -37,39 +37,40 @@ handleUserInfoChange(event) {                 // Signing in to Landing Page hand
   
   handleUserInfoSubmit(event) {                // Handling Submit of user info to find/create a User in Backend => sets session to user.id
     event.preventDefault();
-    let name = this.state.name;
-    let email = this.state.email;
-    fetch('http://localhost:9000/login', {
-      method: 'POST',
-      credentials: "include",
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify({
-        name: this.state.name,
-        email: this.state.email,
-        password: this.state.password
+     let name = this.state.name;
+     let email = this.state.email;
+       fetch('http://localhost:9000/login', {
+         method: 'POST',
+         credentials: "include",
+         headers: {
+          'Content-Type': 'application/json',
+           Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password
+       })
       })
-      })
-    .then(res => res.json())
-    .then(user=> {
-      if(user.error) {
+     .then(res => res.json())
+     .then(user=> {
+      if(user.error){
         alert(user.error)
       } else {
-        this.props.getCurrentUser()  // Submits user info to backend & starts a current_user in Session. Then Dispatch the Action Creator function to Redux Store
+         this.props.getCurrentUser()  // Submits user info to backend & starts a current_user in Session. Then Dispatch the Action Creator function to Redux Store
       }
     })
     .then(() => {
-    localStorage.setItem('Name', name)
-    localStorage.setItem('Email',email)
-    window.location= "http://localhost:8888/login"}) // Transports to Express Server on 8888 which is the defined callback route for Spotify to grant Access Token & route back to localhost:3000
+      localStorage.setItem('Name', name)
+      localStorage.setItem('Email',email)  // Transports to Express Server on 8888 which is the defined callback route for Spotify to grant Access Token & route back to localhost:3000
+      window.location= "http://localhost:8888/login"
+    }) 
     .catch(err=> alert(err))
-    this.setState({
-      name: '',
-      email: '',
-      password: ''
-    });
+        this.setState({
+          name: '',
+          email: '',
+          password: ''
+     });
   }
 
 
@@ -78,30 +79,31 @@ render() {
       <div>
         {this.artBackGround()}
         <h1 style={{color:'navajowhite', textDecoration:'underline dashed'}}>NYC Bands Showcase</h1>
-              <p>Hey there, please sign in/up below!</p>
+          <p>Hey there, please sign in/up below!</p>
             <form onSubmit={(event) => this.handleUserInfoSubmit(event)} style={{border: '1pt solid white'}}>
-          <label>Email: &nbsp; </label>
-          <input
-            type="text" name="email"
-            value={this.state.email}
-            onChange={(event) => this.handleUserInfoChange(event)} />
-            <br/>
-            <label>Name:&nbsp; </label>
-            <input
-            type="text" name="name"
-            value= {this.state.name}
-            onChange={(event)=> this.handleUserInfoChange(event)} />
-            <br/>
-            <label>Password:</label>
-            <input
-            type="password" name="password"
-            value= {this.state.password}
-            onChange={(event)=> this.handleUserInfoChange(event)} />
-          <input type="submit" />
-        </form>
-        </div>)
-    }
+              <label>Email: &nbsp; </label>
+                <input
+                  type="text" name="email"
+                  value={this.state.email}
+                  onChange={(event) => this.handleUserInfoChange(event)} />
+                <br/>
+              <label>Name:&nbsp; </label>
+                <input
+                type="text" name="name"
+                value= {this.state.name}
+                onChange={(event)=> this.handleUserInfoChange(event)} />
+              <br/>
+              <label>Password:</label>
+                <input
+                type="password" name="password"
+                value= {this.state.password}
+                onChange={(event)=> this.handleUserInfoChange(event)} />
+              <input type="submit" />
+            </form>
+      </div>
+    )
   }
+}
 
 const mapStateToProps = state => {          
     return {          // Access to  CurrentUser in Props          
