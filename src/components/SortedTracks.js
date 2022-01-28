61e886c6eef8c9527c7e8409675a43698dbac96b
Benjamin Parish <parishben13@gmail.com>
2022-01-27 23:36:04 -0500

    Refined SortedTracks Component

--- src/components/SortedTracks.js
+++ src/components/SortedTracks.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-export const SortedTracks = ({ toptracks, handleClick})=> {
+export const SortedTracks = ({ toptracks, handleClick}) => {
 
-
-    const renderTracks = toptracks && toptracks.map((track, index) => {
-        if (track !== undefined)  return   <li key={track.name}> <button key={index} onClick={(event)=> handleClick(event)}>{track.name}</button></li>       // checks for toptracks in this.props Then maps through tracks to create buttons    
-     })
-
-    return (
-        <>
-            {renderTracks}
-        </>
-    )
+         const renderTracks = toptracks && toptracks.map((track, index) => {
+            if(track !== undefined)  
+              return <li key={track.name}> <button key={index} onClick={(event)=> handleClick(event)}>{track.name}</button></li>       // checks for toptracks in this.props Then maps through tracks to create buttons    
+            })
+    
+         return(
+                <>
+                 {renderTracks}
+                </>
+              )
 }
