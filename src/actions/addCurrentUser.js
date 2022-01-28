1427d9de7971d9e00ac74e7520788f2004045020
Benjamin Parish <parishben13@gmail.com>
2022-01-28 00:27:06 -0500

    Refined AddCurrentUser Action

--- src/actions/addCurrentUser.js
+++ src/actions/addCurrentUser.js
@@ -1,20 +1,21 @@
   export const getCurrentUser = () => {
       return (dispatch) => {
         fetch('http://localhost:9000/currentuser', {     //Set up backend route to receive currentuser to sessions#get_current_user
-        credentials: "include",    
-        headers: {
-                "Content-type": 'application/json',
-                Accept: 'application/json'
-                }
-             })
-            .then(resp => resp.json())
-            .then(user => {
-                if(user.error){
-                    throw new Error (user.error)
-                } if(!user.error) {
-                dispatch({type: 'CURRENT_USER', currentUser: user})   //Dispatching to Redux the Serialized User data from Backend.
-                } 
-             })
-          .catch(err=> console.log(err))
+          credentials: "include",    
+            headers: {
+                  "Content-type": 'application/json',
+                  Accept: 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(user => {
+          if(user.error){
+              throw new Error (user.error)
+          } 
+          if(!user.error){
+              dispatch({type: 'CURRENT_USER', currentUser: user})   //Dispatching to Redux the Serialized User data from Backend.
+          } 
+        })
+        .catch(err=> console.log(err))
       }
     }  
